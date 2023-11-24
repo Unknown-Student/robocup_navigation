6422132927c96e06e6615e9e1c008ae10bea686a
Unknown-Student <re22m041@technikum-wien.at>
2023-11-24 11:28:34 +0100

    bugfixing

--- robocup_navigation/camera_module.py
+++ robocup_navigation/camera_module.py
@@ -10,7 +10,7 @@ class Camera(Node):
 
     def __init__(self):
         super().__init__("webcam")
-        self.camera_pub_ = self.create_publisher(Image,"/image_raw",1)
+        self.camera_pub = self.create_publisher(Image,"/image_raw",1)
         self.timer = self.create_timer(0.1,self.timer_callback)
         self.cap = cv2.VideoCapture(0)
         self.cv_bridge = CvBridge()
@@ -20,6 +20,6 @@ class Camera(Node):
         ret, frame = self.cap.read()
 
         if ret == True:
-            self.camera_pub(self.cv_bridge.cv2_to_imgmsg(frame, 'bgr8'))
+            self.camera_pub.publish(self.cv_bridge.cv2_to_imgmsg(frame, 'bgr8'))
 
         self.get_logger().info('Publishing video frame')
