39d52a77c7954200930e2bc653fd24d117292444
Unknown-Student <re22m041@technikum-wien.at>
2023-12-11 19:58:46 +0100

    add launch file for aruco marker

--- launch/aruco.launch.py
+++ launch/aruco.launch.py
@@ -1,11 +1,9 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
    
